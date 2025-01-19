c97a83897d9cac06830223769b374913da2f66f2
Edwin Ramos <edwin_ramos2@usmp.pe>
2025-01-18 23:19:20 -0500

    Add: Configuración de Swagger

---
+++ src/main/java/com/market/minimarket/web/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.market.minimarket.web.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("STORE Spring Boot 3 API")
+                        .version("0.1")
+                        .description("Sample app Spring Boot 3 with Swagger")
+                        .termsOfService("https://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org/")));
+    }
+}
